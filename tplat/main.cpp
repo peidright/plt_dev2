0f9d779f92155a576bc788127023fc22ac7485e1
peidright@163.com <dev@devs-MacBook-Pro.local>
2015-01-29 23:52:02 +0800

    Initial Commit

---
+++ tplat/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tplat
+//
+//  Created by dev on 1/29/15.
+//  Copyright (c) 2015 dev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
